a4ef601e9d583349e96febf94b80288f2e35496b
Андрей <anastasiiapegar@gmail.com>
2020-09-17 11:18:40 +0300

    Updates

---
+++ precache-manifest.430566a0062cd6d50540208ab104b853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb87232e69c16a44cfa3cfbf3f277e0",
+    "url": "/Ignats_HometasksMod/index.html"
+  },
+  {
+    "revision": "0631cd3320cd2d909a12",
+    "url": "/Ignats_HometasksMod/static/css/main.0fe101a0.chunk.css"
+  },
+  {
+    "revision": "81ca4f8138d064e5df9f",
+    "url": "/Ignats_HometasksMod/static/js/2.ea7f42ac.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Ignats_HometasksMod/static/js/2.ea7f42ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0631cd3320cd2d909a12",
+    "url": "/Ignats_HometasksMod/static/js/main.83980806.chunk.js"
+  },
+  {
+    "revision": "8096071f5905489c191e",
+    "url": "/Ignats_HometasksMod/static/js/runtime-main.4f05ff87.js"
+  }
+]);
